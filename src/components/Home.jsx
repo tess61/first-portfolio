9d2a19c27c8247d67f47e4a5f0a61447418f5741
tesfa <hopemelkamu28@gmail.com>
2022-12-02 15:08:20 +0300

    skill updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,13 +12,11 @@ const Home = () => {
       <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
         <div className="flex flex-col justify-center h-full">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Trying to be a Full Stack Developer
+            Junior React Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
-            I May Not Have the Experience Now But I definetly Have The grit To Be One
             Currently, I love to work on web application using technologies like
-            React, Tailwind, Next JS and GraphQL
-            But All This is Just Currently And I Won't Be Bound Just By This .
+            HTML, CSS, Bootstrap, React, Tailwind, & Laravel 
           </p>
 
           <div>
